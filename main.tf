cc76cb4eda6df2f5a415f13f6c5351fa3216c65a
simkibo12 <74437731+simkibo12@users.noreply.github.com>
2022-09-19 13:18:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vpc" {
   network_name                           = var.network_name
   auto_create_subnetworks                = var.auto_create_subnetworks
   routing_mode                           = var.routing_mode
-  project_id                             = "p-w3ixydffszwt-0"
+  project_id                             = var.project_id
   description                            = var.description
   shared_vpc_host                        = var.shared_vpc_host
   delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
